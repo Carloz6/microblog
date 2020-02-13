f3eba9316ba0d16cc35b5a5e4efea1b7baceaaba
Carlos Zepeda <carlos-zepeda@outlook.com>
2020-02-13 16:29:18 -0600

    Login Form Created

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 @app.route('/index')
 def index():
 	user = {'username': 'Carlos'}
